5a7d58bba9861133b3f203de34137d31c2af5822
= <the.digital.avatar@gmail.com>
2023-10-08 17:39:39 -0700

    All but the fiveday because of cost

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -11,10 +11,6 @@ $(document).ready(function() {
           response.json()
       .then(function(data) {
             setAutocomplete(data); //gets and sets autocomplete info
-            //setCurrentWeather(data); //gets and sets current weather info
-            //getFutureWeatherByLatLon(data);
-            //currentweather = getCurrentWeatherByLatLon(data[0].lat, data[0].lon);
-            
       });
         } else {
           console.log('Error: ' + response.statusText);
@@ -25,7 +21,7 @@ $(document).ready(function() {
 
 
   function setAutocomplete(data) {
-    console.log(data);
+    
     var cities = [];
     data.forEach(entry => {cities.push(entry.name + " | " + entry.state + " | " + entry.country)});
      $("#city").autocomplete({ 
@@ -36,32 +32,17 @@ $(document).ready(function() {
       }
      });
    };
-  function setCurrentWeather(data) {
-    var lat = data[0].lat;
-    var lon = data[0].lon;
-    var url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=e74a690a9be35ece3c3d6e4a8361c78f";
-      
-      fetch(url, {
-          method: 'GET'
-        })
-          .then(function(response) {
-            if (response.ok) {
-              response.json()
-          .then(function(data) {
-            return data;
-          });
-            } else {
-              return 'Error: ' + response.statusText;
-            }
-          })
-   };
-  function getFutureWeatherByLatLon(data) { };
+
+
+
+  
   
 
 
 
     
     function getCityByName(cityConcatenated) {
+      //Since I bring it the entire string with Pipes, I'm splitting the string below for the URL call
         var cityName = cityConcatenated.split(" | ")[0];
         var stateName = cityConcatenated.split(" | ")[1];
         var countryName = cityConcatenated.split(" | ")[2];
@@ -76,33 +57,25 @@ $(document).ready(function() {
               if (response.ok) {
                 response.json()
             .then(function(data) {
-                  //var currentweather = {};
-                  console.log(data);
-                  currentweather = getCurrentWeatherByLatLon(data[0].lat, data[0].lon);
-                  //getFutureWeatherByLatLon(data[0].lat, data[0].lon);
-                  console.log(currentweather);
-                  
+                  //Since this function only runs when I select an item from the dropdown, I'm calling the other functions here
+                  getCurrentWeatherByLatLon(data);
+                  getFutureWeatherByLatLon(data);
             });
               } else {
                 console.log('Error: ' + response.statusText);
               }
             })
 
-
+        
 
      }
 
 
-     function getCurrentWeatherByLatLon(lat, lon) {
+     function getCurrentWeatherByLatLon(data) {
       
-          
-     }
-
-
-    function getFutureWeatherByLatLon(lat, lon) { 
-        //var url = "https://api.openweathermap.org/data/2.5/weather?id=524901&appid=e74a690a9be35ece3c3d6e4a8361c78f"
-
-        var url = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=e74a690a9be35ece3c3d6e4a8361c78f";
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      var url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=e74a690a9be35ece3c3d6e4a8361c78f&units=imperial";
         
         fetch(url, {
             method: 'GET'
@@ -111,7 +84,31 @@ $(document).ready(function() {
               if (response.ok) {
                 response.json()
             .then(function(data) {
-                  return data
+              console.log(data);
+              setCurrentWeather(data);
+            });
+              } else {
+                return 'Error: ' + response.statusText;
+              }
+            })
+     }
+
+
+    function getFutureWeatherByLatLon(data) { 
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+
+        
+        var url = "https://api.openweathermap.org/data/2.5/forecast/daily?lat="+lat+"&lon="+lon+"&appid=e74a690a9be35ece3c3d6e4a8361c78f&units=imperial";
+        fetch(url, {
+            method: 'GET'
+          })
+            .then(function(response) {
+              if (response.ok) {
+                response.json()
+            .then(function(data) {
+                  createFiveDay(data.list);
+                  console.log(data);
             });
               } else {
                 console.log('Error: ' + response.statusText);
@@ -120,6 +117,7 @@ $(document).ready(function() {
     }
 
 
+    
     function createFiveDay(data){
 
         data.forEach(element => {
@@ -133,6 +131,7 @@ $(document).ready(function() {
             cardTitle.addClass("card-title");
             var cardText = $("p");
             cardText.addClass("card-text");
+            console.log(data);
             card.append(cardBody);
             cardBody.append(cardTitle);
             cardBody.append(cardText);
@@ -142,6 +141,45 @@ $(document).ready(function() {
 
 
     }
+
+
+
+
+    function setCurrentWeather(data) {
+      var today = new Date();  //Wanted to not use day.js for fun
+      var day = String(today.getDate()).padStart(2, '0');  //Current day as DD
+      var month = String(today.getMonth() + 1).padStart(2, '0');  //Current month as MM
+      var year = today.getFullYear(); //Full year as YYYY
+      today = month + '/' + day + '/' + year;
+
+        $("#cityname").text(data.name);
+        $("#date").text(today);
+        $("#temp").text("Current/high/low Temp: "+ Math.round(data.main.temp) + " 🔼" + Math.round(data.main.temp_max) + " 🔽" + Math.round(data.main.temp_min));
+        $("#humidity").text("Humidity: "+ data.main.humidity);
+        $("#wind").text("Windspeed: "+ data.wind.speed);
+        $("#weatherIcon").attr("src","https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
+        
+        
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     var array = ['one', 'two', 'three', 'four', 'five'];
     
     //getCityByName("Atlanta");
@@ -154,7 +192,7 @@ $(document).ready(function() {
         //getCityByName(city);
     });
 
-    $("#city").keyup(function(e) { 
+    $("#city").on("keyup", function(e) { 
       console.log(e.target.value);
       getAutcompleteInfo(e.target.value);
     });
